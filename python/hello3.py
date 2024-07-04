1e30df8c8db348437ca09d1a2550d9ff541daf5a
황태영 <ghkdxodud10253504@gmail.com>
2024-07-04 17:30:29 +0900

    5일차

--- python/hello3.py
+++ python/hello3.py
@@ -1,11 +1,5 @@
-arr1 = [[1,2],[2,3]]
-arr2 = [[3,4],[5,6]]
+class Family: 
+    lastname = "김씨"
 
-result = [] #리스트 none값 주기
-for i in range(len(arr1)): #2번 반복하라 i = 0, i = 1
-    row = [] #리스트 none값 주기 , 행이라 초기화 작업
-    for j in range(len(arr1[0])): #2번 반복 j = 0, j =1
-        row.append(arr1[i][j] + arr2[i][j])#각 리스트 마다 첫 번쨰, 두 번쨰 열 꺼내서 더해서 행으로 만들기
-    result.append(row) #열 더해서 행으로 만든 값 결과에 넣어주기
-
-print(result)
+a = Family() #김씨
+b = Family() #김씨
