cc07535b62756e2a36cc9a3cc5d0d405cf714af4
황태영 <ghkdxodud10253504@gmail.com>
2024-07-08 00:25:03 +0900

    7일차

---
+++ python/7일차/과제/mymath.py
@@ -0,0 +1,14 @@
+def triangle(a,b):
+    result = (a * b) / 2
+
+    return result
+
+def circle(a):
+    result = 3.14 * (a ** 2)
+
+    return result
+
+def rectan(a, b, c):
+    result = 2 * ((a*b) + (b*c) + (c*a))
+
+    return result
--- python/hello.py
+++
@@ -1,9 +0,0 @@
-def odd_numbers(n: int):
-    for i in range(n + 1):
-        if i % 2 != 0:
-            yield i
-
-# 제너레이터 함수 사용 예시
-odd_list = list(odd_numbers(16))
-
-print(odd_list)
